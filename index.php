085f0e12b6f446a3f2baff90537da5909b9f4634
sharapudinov <sharapudinov@mail.ru>
2014-11-25 03:34:26 +0300

    newproducts

--- index.php
+++ index.php
@@ -2,73 +2,66 @@
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Интернет-магазин \"Одежда\"");
 ?><!--=========== Мини слайдер ========-->
-<section>
+<section xmlns="http://www.w3.org/1999/html">
     <div class="minislider-home">
-        <?GLOBAL $arFilter;
-        $arFilter=array("PROPERTY_SPECIALOFFER"=>3)?>
-        <?$APPLICATION->IncludeComponent("bitrix:catalog.top", "finland-watch-mini-slider", array(
-	"IBLOCK_TYPE" => "catalog",
-	"IBLOCK_ID" => "2",
-	"ELEMENT_SORT_FIELD" => "sort",
-	"ELEMENT_SORT_ORDER" => "desc",
-	"ELEMENT_SORT_FIELD2" => "id",
-	"ELEMENT_SORT_ORDER2" => "desc",
-	"FILTER_NAME" => "arFilter",
-	"HIDE_NOT_AVAILABLE" => "N",
-	"ELEMENT_COUNT" => "5",
-	"LINE_ELEMENT_COUNT" => "5",
-	"PROPERTY_CODE" => array(
-		0 => "SPECIALOFFER",
-		1 => "",
-	),
-	"OFFERS_FIELD_CODE" => array(
-		0 => "PREVIEW_PICTURE",
-		1 => "",
-	),
-	"OFFERS_PROPERTY_CODE" => array(
-		0 => "ARTNUMBER",
-		1 => "COLOR_REF",
-		2 => "SIZES_SHOES",
-		3 => "SIZES_CLOTHES",
-		4 => "MORE_PHOTO",
-		5 => "",
-	),
-	"OFFERS_SORT_FIELD" => "sort",
-	"OFFERS_SORT_ORDER" => "asc",
-	"OFFERS_SORT_FIELD2" => "id",
-	"OFFERS_SORT_ORDER2" => "desc",
-	"OFFERS_LIMIT" => "5",
-	"ROTATE_TIMER" => "30",
-	"SECTION_URL" => "",
-	"DETAIL_URL" => "",
-	"SECTION_ID_VARIABLE" => "SECTION_ID",
-	"CACHE_TYPE" => "A",
-	"CACHE_TIME" => "36000000",
-	"CACHE_GROUPS" => "N",
-	"DISPLAY_COMPARE" => "N",
-	"CACHE_FILTER" => "Y",
-	"PRICE_CODE" => array(
-		0 => "BASE",
-	),
-	"USE_PRICE_COUNT" => "N",
-	"SHOW_PRICE_COUNT" => "1",
-	"PRICE_VAT_INCLUDE" => "Y",
-	"CONVERT_CURRENCY" => "N",
-	"BASKET_URL" => "/personal/basket.php",
-	"ACTION_VARIABLE" => "action",
-	"PRODUCT_ID_VARIABLE" => "id",
-	"USE_PRODUCT_QUANTITY" => "N",
-	"ADD_PROPERTIES_TO_BASKET" => "N",
-	"PRODUCT_QUANTITY_VARIABLE" => "quantity",
-	"PRODUCT_PROPS_VARIABLE" => "prop",
-	"PARTIAL_PRODUCT_PROPERTIES" => "Y",
-	"PRODUCT_PROPERTIES" => array(
-	),
-	"OFFERS_CART_PROPERTIES" => array(
-	)
-	),
-	false
-);?>
+        <? $arFilter = array("PROPERTY_SPECIALOFFER" => 3) ?>
+        <?$APPLICATION->IncludeComponent("bitrix:catalog.top", "finland-watch-specialoffer", array(
+                "IBLOCK_TYPE" => "catalog",
+                "IBLOCK_ID" => "2",
+                "ELEMENT_SORT_FIELD" => "sort",
+                "ELEMENT_SORT_ORDER" => "desc",
+                "ELEMENT_SORT_FIELD2" => "id",
+                "ELEMENT_SORT_ORDER2" => "desc",
+                "FILTER_NAME" => "arFilter",
+                "HIDE_NOT_AVAILABLE" => "N",
+                "ELEMENT_COUNT" => "5",
+                "LINE_ELEMENT_COUNT" => "5",
+                "PROPERTY_CODE" => array(
+                    0 => "",
+                    1 => "",
+                ),
+                "OFFERS_FIELD_CODE" => array(
+                    0 => "PREVIEW_PICTURE",
+                    1 => "",
+                ),
+                "OFFERS_PROPERTY_CODE" => array(
+                    0 => "",
+                    1 => "",
+                ),
+                "OFFERS_SORT_FIELD" => "sort",
+                "OFFERS_SORT_ORDER" => "asc",
+                "OFFERS_SORT_FIELD2" => "id",
+                "OFFERS_SORT_ORDER2" => "desc",
+                "OFFERS_LIMIT" => "5",
+                "ROTATE_TIMER" => "30",
+                "SECTION_URL" => "",
+                "DETAIL_URL" => "",
+                "SECTION_ID_VARIABLE" => "SECTION_ID",
+                "CACHE_TYPE" => "A",
+                "CACHE_TIME" => "36000000",
+                "CACHE_GROUPS" => "N",
+                "DISPLAY_COMPARE" => "N",
+                "CACHE_FILTER" => "Y",
+                "PRICE_CODE" => array(
+                    0 => "BASE",
+                ),
+                "USE_PRICE_COUNT" => "N",
+                "SHOW_PRICE_COUNT" => "1",
+                "PRICE_VAT_INCLUDE" => "Y",
+                "CONVERT_CURRENCY" => "N",
+                "BASKET_URL" => "/personal/basket.php",
+                "ACTION_VARIABLE" => "action",
+                "PRODUCT_ID_VARIABLE" => "id",
+                "USE_PRODUCT_QUANTITY" => "N",
+                "ADD_PROPERTIES_TO_BASKET" => "N",
+                "PRODUCT_QUANTITY_VARIABLE" => "quantity",
+                "PRODUCT_PROPS_VARIABLE" => "prop",
+                "PARTIAL_PRODUCT_PROPERTIES" => "Y",
+                "PRODUCT_PROPERTIES" => array(),
+                "OFFERS_CART_PROPERTIES" => array()
+            ),
+            false
+        );?>
     </div>
 </section>
 <!-- ==============Мини слайдер конец ============== -->
@@ -76,15 +69,15 @@ $APPLICATION->SetTitle("Интернет-магазин \"Одежда\"");
 <section>
     <div id="main-slider">
         <?$APPLICATION->IncludeComponent("webvbi:furniture.catalog.index", "finland-watch-actions-slider", array(
-	"IBLOCK_TYPE" => "news",
-	"IBLOCK_ID" => "4",
-	"IBLOCK_BINDING" => "element",
-	"CACHE_TYPE" => "A",
-	"CACHE_TIME" => "36000",
-	"CACHE_GROUPS" => "N"
-	),
-	false
-);?>
+                "IBLOCK_TYPE" => "news",
+                "IBLOCK_ID" => "4",
+                "IBLOCK_BINDING" => "element",
+                "CACHE_TYPE" => "A",
+                "CACHE_TIME" => "36000",
+                "CACHE_GROUPS" => "N"
+            ),
+            false
+        );?>
         <div class="block-advantages">
             <div class="shadow"></div>
             <div class="advantages">
@@ -106,4 +99,190 @@ $APPLICATION->SetTitle("Интернет-магазин \"Одежда\"");
             <div class="clear"></div>
         </div>
     </div>
-</section><? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
+</section>
+<div id="main-content">
+    <div class="content">
+        <section>
+            <div class="block-option">
+                <div class="select-option one">
+                    <form action="" method="post">
+                        <div class="box visible">
+                            <div class="section">
+                                <div class="sec">
+                                    <select class="select-style" data-placeholder="Выберите серию часов">
+                                        <option></option>
+                                        <option>ВСЕ МОДЕЛИ</option>
+                                        <option>СМАРТ ЧАСЫ</option>
+                                        <option>ТУЗИЗМ</option>
+                                        <option>ФИТНЕЛ</option>
+                                        <option>БЕГ</option>
+                                        <option>ВЕЛОСПОРТ</option>
+                                        <option>ДАЙВИНГ</option>
+                                        <option>СКАЛОЛАЗАНИЕ</option>
+                                        <option>ГОРНЫЕ ЛЫЖИ / СНОУБОРД</option>
+                                        <option>ЯХТИНГ</option>
+                                        <option>ПЛАВАНИЕ</option>
+                                        <option>АКСЕССУАРЫ</option>
+                                        <option>КОМАНДНЫЕ СИСТЕМЫ</option>
+                                        <option>КОМПАСЫ</option>
+                                        <option>СПОРТИВНЫЕ НАБОРЫ</option>
+                                        <option>ПОДАРКИ</option>
+                                        <option>АРХИВ МОДЕЛЕЙ</option>
+                                    </select>
+                                </div>
+                                <!-- .sec -->
+                            </div>
+                            <!-- .box -->
+                        </div>
+                    </form>
+
+                </div>
+                <div class="select-option">
+                    <form action="" method="post">
+                        <div class="box visible">
+                            <div class="section">
+                                <div class="sec">
+                                    <select class="select-style" data-placeholder="Выберите вид спорта">
+                                        <option></option>
+                                        <option>ВСЕ МОДЕЛИ</option>
+                                        <option>СМАРТ ЧАСЫ</option>
+                                        <option>ТУЗИЗМ</option>
+                                        <option>ФИТНЕЛ</option>
+                                        <option>БЕГ</option>
+                                        <option>ВЕЛОСПОРТ</option>
+                                        <option>ДАЙВИНГ</option>
+                                        <option>СКАЛОЛАЗАНИЕ</option>
+                                        <option>ГОРНЫЕ ЛЫЖИ / СНОУБОРД</option>
+                                        <option>ЯХТИНГ</option>
+                                        <option>ПЛАВАНИЕ</option>
+                                        <option>АКСЕССУАРЫ</option>
+                                        <option>КОМАНДНЫЕ СИСТЕМЫ</option>
+                                        <option>КОМПАСЫ</option>
+                                        <option>СПОРТИВНЫЕ НАБОРЫ</option>
+                                        <option>ПОДАРКИ</option>
+                                        <option>АРХИВ МОДЕЛЕЙ</option>
+                                    </select>
+                                </div>
+                                <!-- .sec -->
+                            </div>
+                            <!-- .box -->
+                        </div>
+                    </form>
+
+                </div>
+
+                <div class="select-option">
+                    <form action="" method="post">
+                        <div class="box visible">
+                            <div class="section">
+                                <div class="sec">
+                                    <select class="select-style" data-placeholder="Часы на каждый день">
+                                        <option></option>
+                                        <option>ВСЕ МОДЕЛИ</option>
+                                        <option>СМАРТ ЧАСЫ</option>
+                                        <option>ТУЗИЗМ</option>
+                                        <option>ФИТНЕЛ</option>
+                                        <option>БЕГ</option>
+                                        <option>ВЕЛОСПОРТ</option>
+                                        <option>ДАЙВИНГ</option>
+                                        <option>СКАЛОЛАЗАНИЕ</option>
+                                        <option>ГОРНЫЕ ЛЫЖИ / СНОУБОРД</option>
+                                        <option>ЯХТИНГ</option>
+                                        <option>ПЛАВАНИЕ</option>
+                                        <option>АКСЕССУАРЫ</option>
+                                        <option>КОМАНДНЫЕ СИСТЕМЫ</option>
+                                        <option>КОМПАСЫ</option>
+                                        <option>СПОРТИВНЫЕ НАБОРЫ</option>
+                                        <option>ПОДАРКИ</option>
+                                        <option>АРХИВ МОДЕЛЕЙ</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </section>
+
+    <!-- Стильный селект -->
+    <script src="<?= SITE_TEMPLATE_PATH ?>/js/jquery.formstyler.js"></script>
+    <script>
+        jQuery.noConflict();
+        jQuery(document).ready(function () {
+            jQuery('.select-style').styler();
+        });
+    </script>
+    <section>
+        <div class="bread-crumbs home">
+            <ul class="bread-crumbs-link">
+                <li class="icon-link-home"><a href="#"></a></li>
+                <li>Новинки магазина</li>
+
+            </ul>
+
+        </div>
+        <div class="slider-home">
+            <? $arFilter = array("PROPERTY_NEWPRODUCT" => 1) ?>
+            <?$APPLICATION->IncludeComponent("bitrix:catalog.top", "finland-watch-newproduct", array(
+                    "IBLOCK_TYPE" => "catalog",
+                    "IBLOCK_ID" => "2",
+                    "ELEMENT_SORT_FIELD" => "sort",
+                    "ELEMENT_SORT_ORDER" => "desc",
+                    "ELEMENT_SORT_FIELD2" => "id",
+                    "ELEMENT_SORT_ORDER2" => "desc",
+                    "FILTER_NAME" => "arFilter",
+                    "HIDE_NOT_AVAILABLE" => "N",
+                    "ELEMENT_COUNT" => "5",
+                    "LINE_ELEMENT_COUNT" => "5",
+                    "PROPERTY_CODE" => array(
+                        0 => "",
+                        1 => "",
+                    ),
+                    "OFFERS_FIELD_CODE" => array(
+                        0 => "PREVIEW_PICTURE",
+                        1 => "",
+                    ),
+                    "OFFERS_PROPERTY_CODE" => array(
+                        0 => "",
+                        1 => "",
+                    ),
+                    "OFFERS_SORT_FIELD" => "sort",
+                    "OFFERS_SORT_ORDER" => "asc",
+                    "OFFERS_SORT_FIELD2" => "id",
+                    "OFFERS_SORT_ORDER2" => "desc",
+                    "OFFERS_LIMIT" => "5",
+                    "ROTATE_TIMER" => "30",
+                    "SECTION_URL" => "",
+                    "DETAIL_URL" => "",
+                    "SECTION_ID_VARIABLE" => "SECTION_ID",
+                    "CACHE_TYPE" => "A",
+                    "CACHE_TIME" => "36000000",
+                    "CACHE_GROUPS" => "N",
+                    "DISPLAY_COMPARE" => "N",
+                    "CACHE_FILTER" => "Y",
+                    "PRICE_CODE" => array(
+                        0 => "BASE",
+                    ),
+                    "USE_PRICE_COUNT" => "N",
+                    "SHOW_PRICE_COUNT" => "1",
+                    "PRICE_VAT_INCLUDE" => "Y",
+                    "CONVERT_CURRENCY" => "N",
+                    "BASKET_URL" => "/personal/basket.php",
+                    "ACTION_VARIABLE" => "action",
+                    "PRODUCT_ID_VARIABLE" => "id",
+                    "USE_PRODUCT_QUANTITY" => "N",
+                    "ADD_PROPERTIES_TO_BASKET" => "N",
+                    "PRODUCT_QUANTITY_VARIABLE" => "quantity",
+                    "PRODUCT_PROPS_VARIABLE" => "prop",
+                    "PARTIAL_PRODUCT_PROPERTIES" => "Y",
+                    "PRODUCT_PROPERTIES" => array(),
+                    "OFFERS_CART_PROPERTIES" => array()
+                ),
+                false
+            );?>
+        </div>
+    </div>
+</div>
+
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
--- local/templates/finland-watch/js/jcarousel.responsive.js
+++ local/templates/finland-watch/js/jcarousel.responsive.js
@@ -1,51 +1,49 @@
 jQuery.noConflict();
 
-(function(jQuery) {
-    jQuery(function() {
-        var jcarousel = jQuery('.jcarousel.slider-two');
+jQuery(document).ready(function () {
+    var jcarousel = jQuery('.jcarousel.slider-two');
 
-        jcarousel
-            .on('jcarousel.slider-two:reload jcarousel.slider-two:create', function () {
-                var width = jcarousel.innerWidth();
+    jcarousel
+        .on('jcarousel.slider-two:reload jcarousel.slider-two:create', function () {
+            var width = jcarousel.innerWidth();
 
-                //if (width >= 600) {
-                  //  width = width / 4;
-                //} else if (width >= 350) {
-                //    width = width / 2;
-               // }
+            //if (width >= 600) {
+            //  width = width / 4;
+            //} else if (width >= 350) {
+            //    width = width / 2;
+            // }
 
-                jcarousel.jcarousel('items').css('width', width + 'px');
-            })
-            .jcarousel({
-                wrap: 'circular'
-            });
+            jcarousel.jcarousel('items').css('width', width + 'px');
+        })
+        .jcarousel({
+            wrap: 'circular'
+        });
 
-        jQuery('.jcarousel-control-prev')
-            .jcarouselControl({
-                target: '-=1'
-            });
+    jQuery('.jcarousel-control-prev')
+        .jcarouselControl({
+            target: '-=1'
+        });
 
-        jQuery('.jcarousel-control-next')
-            .jcarouselControl({
-                target: '+=1'
-            });
+    jQuery('.jcarousel-control-next')
+        .jcarouselControl({
+            target: '+=1'
+        });
 
-        jQuery('.jcarousel-pagination')
-            .on('jcarouselpagination:active', 'a', function() {
-                jQuery(this).addClass('active');
-            })
-            .on('jcarouselpagination:inactive', 'a', function() {
-                jQuery(this).removeClass('active');
-            })
-            .on('click', function(e) {
-                e.preventDefault();
-            })
-            .jcarouselPagination({
-                perPage: 1,
-                item: function(page) {
-                    return '<a href="#' + page + '">' + page + '</a>';
-                }
-            });
-    });
-})(jQuery);
+    jQuery('.jcarousel-pagination')
+        .on('jcarouselpagination:active', 'a', function () {
+            jQuery(this).addClass('active');
+        })
+        .on('jcarouselpagination:inactive', 'a', function () {
+            jQuery(this).removeClass('active');
+        })
+        .on('click', function (e) {
+            e.preventDefault();
+        })
+        .jcarouselPagination({
+            perPage: 1,
+            item: function (page) {
+                return '<a href="#' + page + '">' + page + '</a>';
+            }
+        });
+});
 
