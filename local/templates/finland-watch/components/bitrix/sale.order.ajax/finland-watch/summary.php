9a9cb2c8128005454698e16bad525a7a2359bde6
sharapudinov <sharapudinov@mail.ru>
2014-12-04 02:42:54 +0300

    sale.order.ajax ready

--- local/templates/finland-watch/components/bitrix/sale.order.ajax/finland-watch/summary.php
+++ local/templates/finland-watch/components/bitrix/sale.order.ajax/finland-watch/summary.php
@@ -275,98 +275,21 @@ $bShowNameWithPicture = ($bDefaultColumns) ? true : false; // flat to show name 
     ?>
     </tr>
 <? endforeach; ?>
+<tr><td></td><td></td><td><?= GetMessage("SOA_TEMPL_SUM_SUMMARY") ?></td><td></td><td></td><td><?=$arResult["ORDER_TOTAL_PRICE_FORMATED"] ?><span class="rouble">a</span></td></tr>
+<tr><td></td><td></td><td><?= GetMessage("SOA_TEMPL_SUM_DELIVERY") ?></td><td></td><td></td><td><?= $arResult["DELIVERY_PRICE_FORMATED"] ?> <span class="rouble">a</span></td></tr>
+<tr><td></td><td></td><td><?= GetMessage("SOA_TEMPL_SUM_IT") ?> </td><td></td><td></td><td><?= $arResult["ORDER_PRICE_FORMATED"] ?>  <span class="rouble">a</span></td></tr>
 </tbody>
 </table>
 </div>
 
-<div class="bx_ordercart_order_pay">
-    <div class="bx_ordercart_order_pay_right">
-        <table class="bx_ordercart_order_sum">
-            <tbody>
-            <tr>
-                <td class="custom_t1" colspan="<?= $colspan ?>"
-                    class="itog"><?= GetMessage("SOA_TEMPL_SUM_WEIGHT_SUM") ?></td>
-                <td class="custom_t2" class="price"><?= $arResult["ORDER_WEIGHT_FORMATED"] ?></td>
-            </tr>
-            <tr>
-                <td class="custom_t1" colspan="<?= $colspan ?>"
-                    class="itog"><?= GetMessage("SOA_TEMPL_SUM_SUMMARY") ?></td>
-                <td class="custom_t2" class="price"><?= $arResult["ORDER_PRICE_FORMATED"] ?></td>
-            </tr>
-            <?
-            if (doubleval($arResult["DISCOUNT_PRICE"]) > 0) {
-                ?>
-                <tr>
-                    <td class="custom_t1" colspan="<?= $colspan ?>"
-                        class="itog"><?= GetMessage("SOA_TEMPL_SUM_DISCOUNT") ?><? if (strLen($arResult["DISCOUNT_PERCENT_FORMATED"]) > 0): ?> (<? echo $arResult["DISCOUNT_PERCENT_FORMATED"]; ?>)<? endif; ?>
-                        :
-                    </td>
-                    <td class="custom_t2" class="price"><? echo $arResult["DISCOUNT_PRICE_FORMATED"] ?></td>
-                </tr>
-            <?
-            }
-            if (!empty($arResult["TAX_LIST"])) {
-                foreach ($arResult["TAX_LIST"] as $val) {
-                    ?>
-                    <tr>
-                        <td class="custom_t1" colspan="<?= $colspan ?>"
-                            class="itog"><?= $val["NAME"] ?> <?= $val["VALUE_FORMATED"] ?>:
-                        </td>
-                        <td class="custom_t2" class="price"><?= $val["VALUE_MONEY_FORMATED"] ?></td>
-                    </tr>
-                <?
-                }
-            }
-            if (doubleval($arResult["DELIVERY_PRICE"]) > 0) {
-                ?>
-                <tr>
-                    <td class="custom_t1" colspan="<?= $colspan ?>"
-                        class="itog"><?= GetMessage("SOA_TEMPL_SUM_DELIVERY") ?></td>
-                    <td class="custom_t2" class="price"><?= $arResult["DELIVERY_PRICE_FORMATED"] ?></td>
-                </tr>
-            <?
-            }
-            if (strlen($arResult["PAYED_FROM_ACCOUNT_FORMATED"]) > 0) {
-                ?>
-                <tr>
-                    <td class="custom_t1" colspan="<?= $colspan ?>"
-                        class="itog"><?= GetMessage("SOA_TEMPL_SUM_PAYED") ?></td>
-                    <td class="custom_t2" class="price"><?= $arResult["PAYED_FROM_ACCOUNT_FORMATED"] ?></td>
-                </tr>
-            <?
-            }
 
-            if ($bUseDiscount):
-                ?>
-                <tr>
-                    <td class="custom_t1 fwb" colspan="<?= $colspan ?>"
-                        class="itog"><?= GetMessage("SOA_TEMPL_SUM_IT") ?></td>
-                    <td class="custom_t2 fwb" class="price"><?= $arResult["ORDER_TOTAL_PRICE_FORMATED"] ?></td>
-                </tr>
-                <tr>
-                    <td class="custom_t1" colspan="<?= $colspan ?>"></td>
-                    <td class="custom_t2"
-                        style="text-decoration:line-through; color:#828282;"><?= $arResult["PRICE_WITHOUT_DISCOUNT"] ?></td>
-                </tr>
-            <?
-            else:
-                ?>
-                <tr>
-                    <td class="custom_t1 fwb" colspan="<?= $colspan ?>"
-                        class="itog"><?= GetMessage("SOA_TEMPL_SUM_IT") ?></td>
-                    <td class="custom_t2 fwb" class="price"><?= $arResult["ORDER_TOTAL_PRICE_FORMATED"] ?></td>
-                </tr>
-            <?
-            endif;
-            ?>
-            </tbody>
-        </table>
-
-        <h4><?= GetMessage("SOA_TEMPL_SUM_COMMENTS") ?></h4>
-
-        <div class="bx_block w100"><textarea name="ORDER_DESCRIPTION" id="ORDER_DESCRIPTION"
-                                             style="max-width:100%;min-height:120px"><?= $arResult["USER_VALS"]["ORDER_DESCRIPTION"] ?></textarea>
-        </div>
-        <input type="hidden" name="" value="">
-    </div>
+<div class="title-head-login">
+    <p><?= GetMessage("SOA_TEMPL_SUM_COMMENTS") ?></p>
 </div>
+<p class="text-info">
+    <textarea name="ORDER_DESCRIPTION" id="ORDER_DESCRIPTION"
+                               style="max-width:100%;min-height:120px"><?= $arResult["USER_VALS"]["ORDER_DESCRIPTION"] ?></textarea>
+</p>
+
+        <input type="hidden" name="" value="">
+
